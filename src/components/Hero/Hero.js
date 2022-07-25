dd847a0d63388a2984a66fafa9d267c5c6269328
Sagar Subedi <sagar@example.com>
2022-07-24 22:32:05 -0500

    feat: hero section completed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi There<br />
+        Welcome to my site...
+      </SectionTitle>
+
+      <SectionText main center>
+        I am a software engineer/web developer with a passion for security.
+      </SectionText>
+
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
