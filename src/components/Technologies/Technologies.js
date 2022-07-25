fb50234af1fec042ebab1913fd3f69258e39c878
Sagar Subedi <sagar@example.com>
2022-07-24 23:14:54 -0500

    feat: technologies section complete

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,51 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiMongodb, DiReact } from 'react-icons/di';
+import { FaNodeJs } from 'react-icons/fa';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+
+    <SectionTitle main>Technologies</SectionTitle>
+
+    <SectionText>
+      I have worked with multiple modern technologies from front-end to back-end.
+    </SectionText>
+
+    <List>
+      <ListItem>
+        <DiReact size="4rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br /> React/NextJS/TailwindCSS
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem>
+        <FaNodeJs size="4rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br /> Node.js/PHP/Python/.NET
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      
+      <ListItem>
+        <DiMongodb size="4rem" />
+        <ListContainer>
+          <ListTitle>Databases</ListTitle>
+          <ListParagraph>
+            Experience with <br /> MongoDB/MySQL/SQLServer
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>      
+    </List>
+  </Section>
 );
 
 export default Technologies;
