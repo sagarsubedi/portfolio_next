574342fbeea972efa347c1608ed2e69f647d2a3f
Sagar Subedi <sagar@example.com>
2022-07-24 23:37:29 -0500

    feat: footer complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,31 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+13304358606">330-435-8606</LinkItem>
+        </LinkColumn>
+
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:subedi.sagar.1212@gmail.com">subedi.sagar.1212@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Building meaningful web apps to make life easier</Slogan>
+        </CompanyContainer>
+
+        <SocialContainer>
+          <SocialIcons href="https://github.com/sagarsubedi">
+            <AiFillGithub size="3rem"/>
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
